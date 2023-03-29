6dc49802f79e344aab76bb55c58e087f2ed34b9b
Fernando Piancastelli <fernando@cellide.com>
2023-03-28 23:38:07 -0300

    Remove onerous call

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const Rake = require('./index.js');
 const path = require('path');
 const fs = require('fs');
 
-const stopwordsPath = path.resolve(`${__dirname}/stopWords.txt`);
+// const stopwordsPath = path.resolve(`${__dirname}/stopWords.txt`);
 
 module.exports = {
   generate(content, opts = {}) {
-    const fileData = fs.readFileSync(stopwordsPath).toString().split('\n');
-    const stopwordsList = opts.stopwords || fileData;
+    // const fileData = fs.readFileSync(stopwordsPath).toString().split('\n'); // onerous call
+    const stopwordsList = opts.stopwords || [];
 
     const instance = new Rake(content, stopwordsList);
     return instance.generate();
